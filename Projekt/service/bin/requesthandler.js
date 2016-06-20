f73f614344141be300d31c057aeb25b5860c8b80
Jan Wegmeth <info@wegmeth.com>
2016-06-20 22:47:12 +0200

    order klein schreiben

---
+++ Projekt/service/bin/requesthandler.js
@@ -0,0 +1,104 @@
+var redis = require('redis');
+var client = redis.createClient();
+
+module.exports = {
+    // findet alle einträge zu einem Key. Benötigt wird ein key (university, faculty,groups ect) und ein Child (university -> faculty) ...
+    findAll: function(req, res, key, child) {
+        //welche HOST ruft mich auf
+        var host = req.headers.host;
+
+        //gibt mir alle Einträge zu einem Key
+        client.keys(key + ':*', function (err, rep) {
+            var object = [];
+            if (rep.length == 0) {
+                res.json(object);
+                return;
+            }
+
+            client.mget(rep, function (err, objectArr) {
+                objectArr.forEach(function (val) {
+                    object.push(JSON.parse(val));
+                });
+                //für jeden treffer müssen wior die Resource jetzt auffrischen
+                object.forEach(function (val) {
+                    var children = [];
+                    //wenn es childs gibt, dann wollen wir einen Link generieren, statt nur die ID zu zeigen
+                    if (val[child]) {
+                        val[child].forEach(function (val) {
+                            //der link besteht aus HOST/CHILD/ID
+                            children.push(host + '/' + child + '/' + val);
+                        });
+                        val[child] = children;
+                    }
+                })
+                res.json(object);
+                return;
+            });
+        });
+    },
+    //Finde einen Eintragn anhand einer ID
+    findById: function(req, res, key, child) {
+        //todo: Childs aufwertebn - aus einer ID muss ein Link erstellt werden. Schön wäre es, dafür eine MEthode zu haben. In diese Mehtode könnte ich ein Array von IDs rein stecken und bekomme ein Array von Links wider !!!!
+        client.get(key + ':' + req.params.id, function(err,rep) {
+            if (rep) {
+                res.type('json').send(rep);
+            }
+            else {
+                res.status(404).type('text').send('Die Uni mit der ID ' +
+                    req.params.id +
+                    ' wurde nicht gefunden');
+            };
+        });
+    },
+
+    //Legt einen Eintrag neu an. gedacht für alle Ressourcen die von einer Parent Ressource abgehen. DH Wenn ich eine Faculty anlegen möchte, muss ich wissen für welche Uni
+    postParentCallback: function (req, res, key, parentkey) {
+        var newObject = req.body;
+
+        //Wenn kein parentKey dann weiß ich nicht wer mein Vater ist, dann kann ich nicht angelegt werden!
+        if (!newObject.hasOwnProperty(parentkey)) {
+            res.status("406").type("text").send("need prop " + parentkey);
+            return;
+        }
+        //prüfe ob mein Vater exestiert. Wenn nicht, kan nich nicht angelegt werden!!
+        client.exists(parentkey + ':' + newObject[parentkey], function (err, reply) {
+            if (reply === 0) {
+                res.status(406).type('text').send('Die ' + parentkey + ' mit der ID ' + req.params.id + ' wurde nicht gefunden');
+                return;
+            }
+            //erzeuge eine ID
+            client.incr('id:' + key, function (err, rep) {
+                newObject.id = rep;
+                client.set(key + ':' + newObject.id, JSON.stringify(newObject), function (err, rep) {
+                    client.get(parentkey + ':' + newObject[parentkey], function (err, rep) {
+                        //mein Parent als JS Objekt, hat entweder schn eine Liste von Childs oder bekommt eine neue liste angelegt
+                        var parent = JSON.parse(rep);
+                        if (parent.hasOwnProperty(key)){
+                            parent[key].push(newObject.id);
+                        }else{
+                            var f = new Array(1);
+                            f[0]=newObject.id;
+                            parent[key] = f;
+                        }
+                        //mein Parent aktualisieren!
+                        client.set(parentkey +':'+ parent.id, JSON.stringify(parent), function(err, rep) {
+                            res.status("201").type("text").send(JSON.stringify(newObject.id));
+                            return;
+                        });
+                    });
+                });
+             });
+        });
+    },
+
+    //Wenn ich keinen Parent habe, dann ist alles viel einfacher! 
+    postCallback: function (req, res, key) {
+        var object = req.body;
+        client.incr('id:'+key, function(err,rep){
+            object.id = rep;
+            client.set(key+':' + object.id, JSON.stringify(object), function(err, rep){
+                res.status(200).type('text').send("created: :" +  object.id);
+            });
+        });
+    }
+}
---
+++ Projekt/service/routes/faculty.js
@@ -0,0 +1,78 @@
+var requesthandler = require('../bin/requesthandler');
+
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+var redis = require('redis');
+var client = redis.createClient();
+var app = express();
+app.use(bodyParser.json());
+
+router.post('/', function(req, res) {
+  requesthandler.postParentCallback(req, res, 'faculty', 'university');
+});
+
+router.get('/', function(req, res) {
+  console.log("Gib alle existierenden Fakultaeten aus...");
+  client.keys('faculty:*', function(err, rep) {
+    var fakultaeten = [];
+    if (rep.length == 0) {
+      res.json(fakultaeten);
+      return;
+    }
+    client.mget(rep, function(err, rep) {
+      rep.forEach(function(val) {
+        fakultaeten.push(JSON.parse(val));
+      });
+      res.json(fakultaeten);
+    });
+  });
+});
+
+router.get('/:id', function(req, res){
+  client.get('faculty:' + req.params.id, function(err,rep) { //Hole den fakultaet mit der bestimmten ID
+    if (rep) {
+      res.type('json').send(rep); //Ist ja schon ein String
+    }
+    else {
+      res.status(404).type('text').send('Die Fakultaet mit der ID ' +
+      req.params.id +
+      ' wurde nicht gefunden');
+    };
+  });
+});
+
+router.put('/:id', function(req,res) {
+  console.log("Aktualisiere eine existierende Fakultaet...");
+  client.exists('faculty:' + req.params.id, function(err, rep) {
+    if (rep == 1) {
+      var updatedfakultaet = req.body;
+      updatedfakultaet.id = req.params.id;
+      client.set('faculty:' + req.params.id, JSON.stringify(updatedfakultaet), function(err, rep) {
+        res.json(updatedfakultaet);
+      });
+    }
+    else {
+      res.status(404).type('text').send('Die Fakultaet mit der ID ' +
+      req.params.id +
+      ' existiert nicht.');
+    }
+  });
+});
+
+router.delete('/:id', function(req, res) {
+  console.log("Lösche eine existierende Fakultaet...");
+  client.del('faculty:' +req.params.id, function(err, rep) {
+      if (rep == 1) {
+        res.status(200).type('text').send('OK');
+      }
+      else {
+        res.status(404).type('text').send('Die Fakultaet mit der ID ' +
+        req.params.id +
+        ' existiert nicht.');
+      }
+  });
+});
+
+module.exports = router;
