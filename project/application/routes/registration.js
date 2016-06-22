ba5747d2b935815c6e2c80130a0603df06f125d9
Jan Wegmeth <info@wegmeth.com>
2016-06-22 17:34:09 +0200

    user registrierung

--- project/application/routes/registration.js
+++ project/application/routes/registration.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('registration', { title: 'Registrierung' });
+  res.render('registration2', { title: 'Registrierung' });
 });
 
 module.exports = router;
