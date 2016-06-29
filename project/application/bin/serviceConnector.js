022df348a69d0e6707cf74bec568ef79c2b53fe7
Jan Wegmeth <info@wegmeth.com>
2016-06-29 23:25:39 +0200

    get

--- project/application/bin/serviceConnector.js
+++ project/application/bin/serviceConnector.js
@@ -1,4 +1,4 @@
-var http = require('http');
+var http = require ('http');
 
 module.exports = {
     sendPost: function (path, data, calbackOnData) {
@@ -10,23 +10,44 @@ module.exports = {
             headers: {
                 accept: "application/json",
                 "Content-Type": "application/json",
-                "Content-Length": Buffer.byteLength(data)
+                "Content-Length": Buffer.byteLength (data)
             }
         };
-        var externalRequest = http.request(options, function (res) {
-            console.log('STATUS: ' + res.statusCode);
-            console.log('HEADERS: ' + JSON.stringify(res.headers));
-            res.setEncoding('utf8');
+        var externalRequest = http.request (options, function (res) {
+            console.log ('STATUS: ' + res.statusCode);
+            console.log ('HEADERS: ' + JSON.stringify (res.headers));
+            res.setEncoding ('utf8');
 
-            res.on('data', calbackOnData);
+            res.on ('data', calbackOnData);
 
         });
 
-        externalRequest.on('error', function (e) {
-            console.log('ERROR: ' + e.message);
+        externalRequest.on ('error', function (e) {
+            console.log ('ERROR: ' + e.message);
         });
 
-        externalRequest.write(data);
-        externalRequest.end();
+        externalRequest.write (data);
+        externalRequest.end ();
+    },
+
+    sendGet: function (path, calbackOnData) {
+        var options = {
+            host: "localhost",
+            port: 3000,
+            path: path,
+            method: "GET",
+        };
+        var externalRequest = http.request (options, function (res) {
+            console.log ('STATUS: ' + res.statusCode);
+            console.log ('HEADERS: ' + JSON.stringify (res.headers));
+            res.setEncoding ('utf8');
+            res.on ('data', calbackOnData);
+        });
+
+        externalRequest.on ('error', function (e) {
+            console.log ('ERROR: ' + e.message);
+        });
+
+        externalRequest.end ();
     }
 }
---
+++ project/application/routes/login.js
@@ -0,0 +1,3 @@
+/**
+ * Created by jan on 29.06.16.
+ */
